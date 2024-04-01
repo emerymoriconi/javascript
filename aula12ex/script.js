495888789946bd21b55dbbffc9e3cd5b65a18c26
Émery Freitas Moriconi <emerymoriconi09@gmail.com>
2024-03-31 21:25:32 -0300

    Exercício 2 - Hora do Dia

--- aula12ex/script.js
+++ aula12ex/script.js
@@ -1,1 +1,18 @@
-alert('Olá!')
+function carregar () {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12) {
+        img.src = 'fotos/manha.jpg'
+        //a cor de fundo muda para a cor do código a depender do horário do dia
+        document.body.style.background = '#e2cd9f'
+    } else if (hora >= 12 && hora <= 18){
+        img.src = 'fotos/tarde.jpg'
+        document.body.style.background = '#B65112'
+    } else {
+        img.src = 'fotos/noite.jpg'
+        document.body.style.background = '#504258'
+    }
+}
