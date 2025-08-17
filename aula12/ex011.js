fdcc2259611a2438fc23dff8b0f809415e224ef5
Émery Freitas Moriconi <emerymoriconi09@gmail.com>
2025-08-17 12:42:13 -0300

    Apagando tudo

--- aula12/ex011.js
+++
@@ -1,9 +0,0 @@
-var idade = 15
-console.log(`Você tem ${idade} anos.`)
-if (idade < 16) {
-    console.log('Não vota.') // no NodeJs o comando de escrita é console.log() e no navegador é window.document.write()
-} else if (idade < 18 || idade > 65) {
-    console.log('Voto opcional.')
-} else {
-    console.log('Voto obrigatório.')
-}
