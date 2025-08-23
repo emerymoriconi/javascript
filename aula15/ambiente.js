a4a1c87ccf205e9fab8eb466c6eeadb976c871e3
Émery Freitas Moriconi <emerymoriconi09@gmail.com>
2025-08-23 12:31:46 -0300

    Módulo F do curso do Guanabara de JS

---
+++ aula15/ambiente.js
@@ -0,0 +1,32 @@
+let num = [5, 8, 2, 9, 3]
+
+num.push(0)
+let tam = num.length
+
+console.log(num)
+
+let sorted = num.sort()
+
+console.log(tam)
+console.log(sorted)
+
+
+// FOR NORMAL
+
+for (let pos = 0; pos < num.length; pos++){
+    console.log(num[pos])
+}
+
+// FOR IN
+// só funciona pra Arrays
+
+for (let pos in num){
+    console.log(num[pos])
+}
+
+// BUSCAR VALORES DENTRO DE UM VETOR
+// devolve o índice daquele valor
+// quando não encontra, devolve -1
+
+let oito = num.indexOf(8)
+console.log(oito)
---
+++ aula16/funcao03.js
@@ -0,0 +1,8 @@
+// JS é uma linguagem funcional
+// uma variavel pode receber uma função!
+
+let v = function(x) {
+    return x*2
+}
+
+console.log(v(5))
